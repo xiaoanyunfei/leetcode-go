3575f6061426ecb030492180f679576a22916e2c
xiaofei.sun <xiaoanyunfei@gmail.com>
2017-03-16 08:01:55 +0800

    add 74

---
+++ 74Search2DMatrix.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	m := len(matrix)
+	if m == 0 {
+		return false
+	}
+	n := len(matrix[0])
+	i, j := m-1, 0
+	for i >= 0 && j < n {
+		if matrix[i][j] > target {
+			i--
+		} else if matrix[i][j] < target {
+			j++
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	matrix := [][]int{
+		{1, 3, 5, 7},
+		{10, 11, 16, 20},
+		{23, 30, 34, 50},
+	}
+	fmt.Println(searchMatrix(matrix, 3))
+}
